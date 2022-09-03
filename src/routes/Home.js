0bbca6cadd6f59cf90a30cd09165926a03bfeecf
Raunak Jodhawat <raunakjodhawat@gmail.com>
2022-09-03 13:07:48 +0530

    Smoother flow for fb login/logout

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,42 +1,72 @@
 /*global FB*/
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { FBModule } from '../utils/fb';
 
-function Home() {
-    let navigate = useNavigate();
-        
-    const [userName, setUsername] = useState("");
-    const [userId, setUserId] = useState(0);
-    window.fbAsyncInit = function () {
+const initFB = () => {
+    window.fbAsyncInit = () => {
         FB.init({
             appId: process.env.FB_APP_SECRET,
             cookie: true,
             xfbml: true,
             version: 'v14.0'
         });
+    };
+}
 
-        FB.AppEvents.logPageView();
-        FB.getLoginStatus(function (response) {
-            console.log(response);
-            if (
-                !response ||
-                (response && !response.status) || 
-                (response && response.status && response.status !== 'connected')) {
-                navigate("/");
+const checkLoginStatus = (cb) => {
+    // Session has not expired
+    if ((localStorage.getItem("Session") && window.atob(localStorage.getItem("Session")) > (Date.now() - 86400))) {
+        cb(null);
+    } else {
+        // Session has expired
+        FB.getLoginStatus((response) => {
+            if (response && response.status && response.status === 'connected') {
+                cb(null);
+            } else {
+                cb("redirect");
             }
         });
+    }
+}
 
-        FB.api('/me', function(response) {
-            setUsername(response.name);
-            setUserId(response.id);
-            console.log(userId);
-        });
-    };
+const getUserNameAndID = (cb) => {
+    FB.api('/me', (response) => {
+        cb({name: response.name, userId: response.id});
+    });
+}
 
-    
+const logOutUser = (cb) => {
+    if (localStorage.getItem("Session")) {
+        localStorage.removeItem("Session");
+    }
+    FB.logout(() => {
+        cb();
+    });
+}
+
+function Home() {
+    let navigate = useNavigate();
+    const [userName, setUsername] = useState("");
+    const [userId, setUserId] = useState(0);
+
+    if (typeof FB === 'undefined') {
+        initFB();
+    }
+    checkLoginStatus((err) => {
+        if (err !== null) {
+            navigate("/");
+        } else {
+            getUserNameAndID((res) => {
+                setUsername(res.name);
+                setUserId(res.userId);
+            })
+        }
+    });
+
 
     const onLogoutButtonClick = () => {
-        FB.logout(function(response) {
+        logOutUser(() => {
             navigate("/");
         });
     }
